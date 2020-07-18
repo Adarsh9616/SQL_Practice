84a3b52f9fc7a610442a0b6a0c4fc037ddd515d9
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-18 22:37:41 +0530

    Update HackerRank.sql

--- HackerRank.sql
+++ HackerRank.sql
@@ -39,3 +39,51 @@ WHERE (id%2)=0;
 --Weather Observation Station 4 
 SELECT COUNT(city)-COUNT(DISTINCT city)
 from station;
+
+--Weather Observation Station 9 
+SELECT DISTINCT city
+from station
+WHERE city LIKE '[^AEIOU]%';
+
+--Weather Observation Station 8 
+SELECT DISTINCT city
+from station
+WHERE city LIKE '[AEIOU]%[AEIOU]';
+
+--Weather Observation Station 7 
+SELECT DISTINCT city
+from station
+WHERE city LIKE '%a' OR city LIKE '%e' OR city LIKE'%i' OR city LIKE '%o' OR city LIKE '%u';
+
+--Weather Observation Station 6 
+SELECT DISTINCT city
+from station
+WHERE city LIKE 'a%' OR city LIKE 'e%' OR city LIKE'i%' OR city LIKE 'o%' OR city LIKE 'u%';
+
+--Weather Observation Station 5 
+select CITY,LENGTH(CITY) from STATION order by Length(CITY) asc, CITY limit 1; 
+select CITY,LENGTH(CITY) from STATION order by Length(CITY) desc, CITY limit 1;
+
+--Employee Names 
+SELECT name
+FROM employee
+ORDER BY name;
+
+--Higher Than 75 Marks 
+SELECT NAME FROM STUDENTS WHERE Marks > 75 ORDER BY RIGHT(NAME, 3), ID ASC;
+
+--Weather Observation Station 12 
+SELECT DISTINCT city
+from station
+WHERE city LIKE '[^AEIOU]%' AND city LIKE '%[^AEIOU]';
+
+--Weather Observation Station 11 
+SELECT DISTINCT city
+from station
+WHERE city LIKE '[^AEIOU]%' OR city LIKE '%[^AEIOU]';
+
+--Weather Observation Station 10 
+SELECT DISTINCT city
+from station
+WHERE city LIKE '%[^AEIOU]';
+
